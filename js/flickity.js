341fb5da06f970970697295e3f0244ccf1d14358
Hubert Jędrzejczak <hubi.mf@gmail.com>
2021-09-23 19:34:51 +0200

    more sections added

--- js/flickity.js
+++ js/flickity.js
@@ -5,8 +5,30 @@ var flkty = new Flickity(elem, {
     contain: true
 });
 
+var rent_elem = document.querySelector('.rental-carousel');
+var flkty = new Flickity(rent_elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+});
+
+var rent_elem = document.querySelector('.explore-carousel');
+var flkty = new Flickity(rent_elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+});
+
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity('.main-carousel', {
+    // options
+});
+
+var flkty = new Flickity('.rental-carousel', {
+    // options
+});
+
+var flkty = new Flickity('.explore-carousel', {
     // options
 });
